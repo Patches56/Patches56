facb59ac905b09ba1d21d148e21660e89593bbf0
Patches56 <133010607+Patches56@users.noreply.github.com>
2024-02-25 23:40:08 -0600

    Add files via upload

---
+++ review_password_validation.py
@@ -0,0 +1,11 @@
+#maya t. week 6, example 6
+#Data validation
+
+user_pw= input("Enter password: ")
+num_attempts = 1
+
+while user_pw != "WCC" and num_attempts <= 3:
+    user_pw= input("Invalid! Enter password: ")
+    num_attempts += 1
+
+print("Login successful")
