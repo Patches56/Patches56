55531e47be1aeffdcd0cc2de472fcc33a72ae6de
Patches56 <133010607+Patches56@users.noreply.github.com>
2024-02-25 23:31:09 -0600

    Add files via upload

---
+++ jg_Assignment_4C.rb
@@ -0,0 +1,60 @@
+#----------------------------------------------------------------------#
+#Assignement:   Assignement 4C
+#Program Name:  Hummingbird Cake 
+#
+#
+#Purose: How To Make a Hummingbird Cake
+#
+#
+#Author:         Jairo Gutierrez
+#Course:         CIS109 - Fall 2024
+#
+#
+#Created:        February 18, 2024
+#
+#----------------------------------------------------------------------
+
+## degrees and measurements
+degree = "\u00b0".force_encoding('utf-8')
+oneHalf = "\u00bd".force_encoding('utf-8')
+blackStar = "\u2605".force_encoding('utf-8')
+whiteStar = "\u2606".force_encoding('utf-8')
+puts
+puts"Hummingbird Cake #{blackStar * 3} #{whiteStar * 1}"
+puts
+puts"This Southern-style Hummingbird layer cake features a crowd-pleasing combination of banana, pineapple, and coconut. The name's origin remains something of a mystery: Some say it's because the cake is as sweet as nectar; others, that it makes you hum with happiness (12 servings)."
+puts
+puts"Ingredients"
+print("3#{} cups all-purpose flour, plus for pans\n")
+print("2#{} cups sugar\n")
+print("1#{} teaspoon table salt\n")
+print("1#{} teaspoon baking soda\n")
+print("3#{} Large eggs, lightly beaten\n")
+print("1#{oneHalf} teaspoon vanilla extract\n")
+print("1#{} 8-oz. can crushed pineapple in juice, undrained\n")
+print("2#{} cups chopped bananas (about 4 meduim bananas)
+  Vegetable shortening\n")
+puts
+puts"Cream Cheese Frosting"
+print("2#{} (8-oz.) packages cream cheese, softened\n")
+print("1#{} cup butter, softened\n")
+print("2#{} (16-oz.) packages powdered sugar\n")
+print("2#{} teaspoons vanilla extract\n")
+puts
+puts"Additional Ingredients"
+print("1#{} cup pecan halves, toasted\n")
+puts
+puts"Directions"
+puts"1. Prepare the Cake Laters:\n
+Preheat oven to 350#{degree}. Whisk together flour, sugar, salt, baking soda, and cinnamon in a large bowl; add eggs and oil, stirring just until dry ingredients are moistened. Stir in vanilla, pineapple, bananas, and toasted pecans.\n"
+puts
+puts"2. Divide batter evenly among 3 well-greased (with shortening) and floured 9-inch round cake pans."
+puts
+puts"3. Bake in the preheated oven until a wooden pick inserted in center comes out clean, 25 to 30 minutes. Cool in pans on wire racks 10 minutes. Remove from pans top wire racks, and cool completely, about 1 hour."
+puts
+puts"4. Prepare the Cream Cheese Frosting:\n"
+puts"Beat cream cheese and butter with an electric mixer on medium-low speed until smooth. Gradually add sugar, beating at low speed until blended after each addition. Stir in vanilla."
+puts"Increase speed to medium-high, and beat until fluffy, 1to 2 minutes."
+puts
+puts"5. Assemble Cake:\n"
+puts"Place first cake layer on a serving platter; spread top with 1 cup f the frosting. Top with second layer, and spread with 1 cup frosting. Top with third layer, and spread remaining frosting over top and sides of cake. Arrange pecan halves on top of cake in circular pattern."
